67db1cb3ec28424955497bc62437ba4fdb8f6332
jan.kraut <info@jankraut.de>
2021-11-29 12:25:05 +0100

    add simple hob

---
+++ jobs/check-if-file-exists.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#NAME="../README.md"
+echo $NAME "file da"
+FILE=$NAME
+if [ -f "$FILE" ]; then
+    echo "$FILE exists."
+    echo "$FILE" > $EXISTS
+    echo $EXISTS
+fi
--- jobs/hello-world.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-input=$1
-echo -e "\e[32m$(cat $input)\e[0m"
-
