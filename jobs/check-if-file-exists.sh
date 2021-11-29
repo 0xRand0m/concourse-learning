60ecca9293c646edb88830a059f87443156ef876
jan.kraut <info@jankraut.de>
2021-11-29 14:43:50 +0100

    update

--- jobs/check-if-file-exists.sh
+++ jobs/check-if-file-exists.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 #NAME="../README.md"
 echo $NAME "file da $NAME"
-echo $NAME1 "file da $NAME1"
+echo $NAME1 "file da name1 $NAME1"
+echo $FILE "file da file $FILE"
 FILE=$NAME
 if [ -f "$FILE" ]; then
     echo "$FILE exists."
